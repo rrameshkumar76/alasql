7f08db201b01ea41917fc5aa8ce6b0b682207b25
rkraja <ramesh.rajagopalan@bayer.com>
2023-08-21 14:49:26 -0500

    fixed

--- src/423groupby.js
+++ src/423groupby.js
@@ -177,11 +177,9 @@ if(false) {
 					} else if (col.aggregatorid === 'MIN') {
 						if ('funcid' in col.expression) {
 							let colexp1 = colExpIfFunIdExists(col.expression);
-							const exp = `'${colas}': (typeof ${colexp1} == 'number' ? ${colexp} : typeof ${colexp1} == 'object' ?
-							typeof Number(${colexp1}) == 'number' && ${colexp1}!== null? ${colexp} : null : null),`;
-							console.log("'" + colas + "':" + colexp + ',')
-							console.log(exp)
-							return exp
+
+							return(`'${colas}': (typeof ${colexp1} == 'number' ? ${colexp} : typeof ${colexp1} == 'object' ?
+							typeof Number(${colexp1}) == 'number' && ${colexp1}!== null? ${colexp} : null : null),`);
 						}
 						return `'${colas}': (typeof ${colexp} == 'number' ? ${colexp} : typeof ${colexp} == 'object' ?
 							typeof Number(${colexp}) == 'number' && ${colexp}!== null? ${colexp} : null : null),`;
@@ -415,10 +413,7 @@ if(false) {
 						if ('funcid' in col.expression) {
 							let colexp1 = colExpIfFunIdExists(col.expression);
 							//console.log(pre + 'if ((y=' + colexp + ") < g['" + colas + "']) g['" + colas + "'])
-							console.log((
-								pre + 'if ((y=' + colexp + ") < g['" + colas + "']) g['" + colas + "'] = y;" + post
-							));
-							const exp =
+							return(
 								pre +
 								`if((g['${colas}'] == null && ${colexp1}!== null) ? y = ${colexp} : (g['${colas}']!== null &&
 							${colexp1} == null) ? y = g['${colas}']:((y=${colexp}) < g['${colas}'])){ if(typeof y == 'number')
@@ -427,9 +422,7 @@ if(false) {
 							else if(g['${colas}']!== null && typeof g['${colas}'] == 'object' && y instanceof Date){g['${colas}'] = g['${colas}']}
 							else if(g['${colas}']!== null && typeof g['${colas}'] == 'object'){g['${colas}'] = Number(g['${colas}'])}` +
 								post
-							;
-							console.log(exp)
-							return  exp
+							);
 						}
 						return (
 							pre +
